cbd4a19b122136109771ba55525ea6b4faa31d4f
franklin-01 <franklinmatos8@gmail.com>
2020-07-30 00:37:08 -0300

    uri_1007

---
+++ C++/uri_1007/uri_1007.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    float A, B, C, D;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cin >> D;
+
+    cout << "DIFERENCA = " << ((A*B)-(C*D)) << endl;
+
+    return 0;
+}
