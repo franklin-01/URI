58ba5f2a1bf261bfa9b3b2416b0b181598008a63
franklin-01 <franklinmatos8@gmail.com>
2020-07-29 23:59:59 -0300

    uri_1004

---
+++ C++/uri_1004/uri_1004.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a, b, PROD;
+    cin >> a;
+    cin >> b;
+    PROD = a*b;
+    cout << "PROD =" << PROD << endl;
+
+    return 0;
+}
